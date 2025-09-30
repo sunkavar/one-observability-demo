635a34ab5830b82035e137be15736632fff42a70
Imaya Kumar Jagannathan <40274599+awsimaya@users.noreply.github.com>
2025-09-30 10:45:03 -0400

    Fix for cleanupadoptions URL (#413)
    * Fix debug-config issue
    
    * Fix an issue with userId
    
    * Waggle AI welcome message change and more
    
    * Change Cleanup url path
    
    ---------
    
    Co-authored-by: Rodrigue Koffi <bonclay7@users.noreply.github.com>

--- src/applications/microservices/petsite-net/petsite/Controllers/HomeController.cs
+++ src/applications/microservices/petsite-net/petsite/Controllers/HomeController.cs
@@ -75,21 +75,15 @@ namespace PetSite.Controllers
         public async Task<IActionResult> HouseKeeping()
         {
             if (EnsureUserId()) return new EmptyResult();
-            
             var userId = ViewBag.UserId?.ToString();
 
             _logger.LogInformation($"In Housekeeping, trying to reset the app for user: {userId}");
-            var userId = ViewBag.UserId?.ToString();
-            
             try
             {
                 string cleanupadoptionsurl = ParameterNames.GetParameterValue(ParameterNames.CLEANUP_ADOPTIONS_URL, _configuration);
-
                 using var httpClient = _httpClientFactory.CreateClient();
-                var userId = ViewBag.UserId?.ToString();
-                var url = UrlHelper.BuildUrl(cleanupadoptionsurl, null, ("userId", userId));
-                var response = await httpClient.PostAsync(url, null);
-
+                var url = UrlHelper.BuildUrl(cleanupadoptionsurl, new String[]{userId}, null);
+                var response = await httpClient.DeleteAsync(url);
                 if (!response.IsSuccessStatusCode)
                 {
                     _logger.LogWarning($"Housekeeping API returned - {response.StatusCode} - for user: {userId}");
@@ -101,6 +95,8 @@ namespace PetSite.Controllers
                 _logger.LogError(e, $"Error calling Housekeeping API: {e.Message} - for user: {userId}");
                 ViewBag.ErrorMessage = $"Unable to perform housekeeping at this time. Please try again later.\nError message: {e.Message}";
             }
+
+            _logger.LogInformation($"Housekeeping complete for user: {userId}");
 
             _logger.LogInformation($"Housekeeping complete for user: {userId}");
 
